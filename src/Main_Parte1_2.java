0c916ba4427fde7009660e72da12b543e3f160ab
Giacomo-Farroni <1997giacomo@gmail.com>
2023-12-19 17:59:46 +0100

    Aggiunta esercizi 7 e 8 ( sul Main_Parte1-2 )

--- src/Main_Parte1_2.java
+++ src/Main_Parte1_2.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;													//classe di riferimento per l'uso del
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;													//imposta classe SqlException per gestione errori nella comunicazione col database
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main_Parte1_2 {
@@ -91,25 +93,128 @@ public class Main_Parte1_2 {
 		    	System.out.println("Il cliente ricercato non � presente!!!");
 		    }
 		    
+			
+		    /* --- PARTE 2 ---
+		    7) Scrivere le istruzioni per la SELECT con LIKE che ritorna l'elenco di
+		    prodotti di tipo televisore
+		    
+		     */
+		    		
+		    		
+		    // 7) Scrivere le istruzioni per la SELECT con LIKE che ritorna l'elenco di
+		    // prodotti di tipo televisore
+		    
+		    String selectFromProdottobyTelevisore =							
+	    			" SELECT codice_Prodotto, descrizione, quantita_disponibile, prezzo "
+	              + "   FROM prodotto                    "
+	              + "  WHERE prodotto.codice_Prodotto LIKE ?  ";
+		    
+		    String parametroTelevisore = "TV%";
+		    
+		    preparedStatement =								
+            		jdbcConnectionToDatabase.prepareStatement(selectFromProdottobyTelevisore);
+            preparedStatement.setString(1, parametroTelevisore);
+		    
+            rsSelect
+            = preparedStatement.executeQuery();								
+
+            List <Prodotto> televisori = new ArrayList<>();
+            
+            while (rsSelect.next()) {
+            	
+            	 String codiceProdotto 
+	        		= rsSelect.getString("codice_Prodotto");		
+	        if (rsSelect.wasNull()) {
+	            codiceProdotto = "";
+	        }
+	        
+	        
+	       int prezzo 
+ 				= rsSelect.getInt("prezzo");
+	       if (rsSelect.wasNull()) {
+	    	   prezzo = 0;}
+ 		
+	       
+		  int quantitaDisponibile 
+					=rsSelect.getInt("quantita_disponibile");
+			 if (rsSelect.wasNull()) {
+	            quantitaDisponibile = 0;}
+	        
+	        
+	        String descrizione												
+	                = rsSelect.getString("descrizione");
+	        if (rsSelect.wasNull()) {
+	            descrizione = "";
+	        } 
+            	
+	        televisori.add(new Prodotto(codiceProdotto, descrizione, prezzo, quantitaDisponibile));
+            	
+            }
+		    
+            if(!televisori.isEmpty())
+            	System.out.println("/nRisultati trovati: " + televisori.toString());
+            else
+            	System.out.println("Nessun risultato trovato");
 		    
 		    
+            /* 8) Inserire un nuovo prodotto di tipo televisore
 		    
-		    
+		    */
+                      
+          
+            
+            
+            String insertFromProdottobyTelevisore =							
+	    			" INSERT INTO prodotto (codice_prodotto, descrizione, quantita_disponibile, prezzo "
+	              + "   VALUES ( ?,?,?,?)                ";
+            
+           /* String insertCodice =" Tv100";
+            String insertDescrizione = "Smart-tv";
+            int insertQuantita = 10;
+            float insertPrezzo = 500;
+            
+            preparedStatement =								
+            		jdbcConnectionToDatabase.prepareStatement(insertFromProdottobyTelevisore);
+            preparedStatement.setString(1, insertCodice);
+            preparedStatement.setString(2, insertDescrizione);
+            preparedStatement.setInt(3, insertQuantita);
+            preparedStatement.setFloat(4, insertPrezzo); */
+            
+            Prodotto p=new Prodotto("TV100", "Smart-Tv", 10, 500);
+	              
+            preparedStatement =								
+            		jdbcConnectionToDatabase.prepareStatement(insertFromProdottobyTelevisore);
+            preparedStatement.setString(1, p.getCodiceProdotto());
+            preparedStatement.setString(2, p.getDescrizione());
+            preparedStatement.setInt(3, p.getQuantitaDisponibile());
+            preparedStatement.setFloat(4, p.getPrezzo());
+           
+            preparedStatement.executeUpdate();
+            
+          System.out.println("prodotto inserito " + p.toString());
+
+            
+            
+            
+            // 9) Ricercare tutti i prodotti di tipo televisor
+            
+            
+            
+            
+            
 		} catch (SQLException e) {												//errore di tipo classe SQLException
 			// TODO Auto-generated catch block
 			e.printStackTrace();												//stampa la pila (stack) degli errori, dal pi� recente al meno recente
 		}
 		
 		
-/* --- PARTE 2 ---
-6) Aggiungere al bean Prodotto i campi della omonima tabella:
-"quantita_disponibile" e "prezzo" ed aggiornare la SELECT coi due
-nuovi campi
-7) Scrivere le istruzioni per la SELECT con LIKE che ritorna l'elenco di
-prodotti di tipo televisore */
 		
 		
 		
+		
+		
+
+		 
 		
 
 		
@@ -139,6 +244,9 @@ prodotti di tipo televisore */
 		
 		
 		
-	}
+	} 
+	
+	
+	
 	
 }
--- src/Prodotto.java
+++ src/Prodotto.java
@@ -6,6 +6,7 @@ public class Prodotto {
 	int quantitaDisponibile;
 	int prezzo;
 	
+
 	
 	
 	public String getCodiceProdotto() {
